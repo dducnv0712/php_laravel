1d40aed51f60a2030710c6778431d31a881fd694
ducbe <ducbe2k2@gmail.com>
2021-07-03 19:13:19 +0700

    PHP LARAVEL

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
@@ -20,4 +20,16 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
+
 }
+
+//namespace App\Http\Middleware;
+//
+//use Illuminate\Http\Request;
+//use Fideloper\Proxy\TrustProxies as Middleware;
+//
+//class TrustProxies extends Middleware
+//{
+//    protected $proxies = '*';
+//    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
+//}
